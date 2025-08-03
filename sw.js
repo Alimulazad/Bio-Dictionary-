1045a36f5e443391dc97c80f650c7ac6d229fb98
Alimulazad <alimulazad5@gmail.com>
2025-08-03 21:02:57 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-sw
+//sw
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('biology-dictionary-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/dictionary(3).js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
